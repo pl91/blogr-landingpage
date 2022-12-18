34d9a6bd7df547764180f4f825c9aad3faea3154
Paul Liani <pliani91@gmail.com>
2022-12-18 12:14:04 -0500

    complete

--- index.579125c3.js
+++ index.579125c3.js
@@ -1,2 +1,13 @@
+// save nav variable
+var nav = document.querySelector(".nav");
+// add mobile-menu class to header 
+function mobileMenu() {
+    if (nav.className === "nav") nav.className += " mobile-menu";
+    else nav.className = "nav";
+}
+// close mobile menu on resize 
+function reSize() {
+    nav.className = "nav";
+}
 
 //# sourceMappingURL=index.579125c3.js.map
