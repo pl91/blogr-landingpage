963c8a46d1479eb9d14cb2c30baea6319a55a6c7
Paul Liani <pliani91@gmail.com>
2022-12-19 14:00:41 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// save nav variable
+// save nav and icon variables
 var nav = document.querySelector(".nav");
+var icon = document.querySelector(".icon");
 
-// add mobile-menu class to header 
+// add mobile-menu class to header and change icon source 
 function mobileMenu() {
   if (nav.className === "nav") {
     nav.className += " mobile-menu";
+    icon.src = "icon-close.8b0baa94.svg";
   } else {
     nav.className = "nav";
+    icon.src = "icon-hamburger.9b6ee044.svg";
   }
 }
 
-// close mobile menu on resize 
+// close mobile menu on and reset icon resize 
 function reSize() {
   nav.className = "nav";
+  icon.src = "icon-hamburger.9b6ee044.svg";
 }
